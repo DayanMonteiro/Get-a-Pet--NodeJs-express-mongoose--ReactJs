6fe124e045a7359b8e09bb82ef5913232e6a704e
DayanMonteiro <dmsmamb@gmail.com>
2023-12-09 10:02:16 -0300

    user validation

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -2,6 +2,55 @@ const User = require("../models/User");
 
 module.exports = class UserController {
   static async register(req, res) {
-    res.json("Olá Get a Pet");
+    // const name = req.body.name;
+    // const email = req.body.email;
+    // const phone = req.body.phone
+    // const password = req.body.password
+    // const confirmpassword = req.body.confirmpassword
+
+    const { name, email, phone, password, confirmpassword } = req.body;
+
+    // validations
+    if (!name) {
+      res.status(422).json({ message: "O nome é orbigatório" });
+      return;
+    }
+
+    if (!email) {
+      res.status(422).json({ message: "O e-mail é obrigatório!" });
+      return;
+    }
+
+    if (!phone) {
+      res.status(422).json({ message: "O telefone é obrigatório!" });
+      return;
+    }
+
+    if (!password) {
+      res.status(422).json({ message: "A senha é obrigatória!" });
+      return;
+    }
+
+    if (!confirmpassword) {
+      res
+        .status(422)
+        .json({ message: "A confirmação de senha é obrigatória!" });
+      return;
+    }
+
+    if (password != confirmpassword) {
+      res
+        .status(422)
+        .json({ message: "A senha e a confirmação precisam ser iguais!" });
+      return;
+    }
+
+    // check if user exists
+    const userExists = await User.findOne({ email: email });
+
+    if (userExists) {
+      res.status(422).json({ message: "Por favor, utilize outro e-mail!" });
+      return;
+    }
   }
 };
