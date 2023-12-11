a0ac553e6711a41ca7276ce9f312d636d05ef7f2
DayanMonteiro <dmsmamb@gmail.com>
2023-12-11 10:28:21 -0300

    creating a user with an encrypted password

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 module.exports = class UserController {
   static async register(req, res) {
@@ -51,6 +52,25 @@ module.exports = class UserController {
     if (userExists) {
       res.status(422).json({ message: "Por favor, utilize outro e-mail!" });
       return;
+    }
+
+    // create a password
+    const salt = await bcrypt.genSalt(12);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    // create user
+    const user = new User({
+      name,
+      email,
+      phone,
+      password: passwordHash,
+    });
+
+    try {
+      const newUser = await user.save();
+      res.status(201).json({ message: "Usuário criado com sucesso!", newUser });
+    } catch (error) {
+      res.status(500).json({ message: error });
     }
   }
 };
