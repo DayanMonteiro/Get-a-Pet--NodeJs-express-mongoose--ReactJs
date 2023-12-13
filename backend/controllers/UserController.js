7478b2b266453390dfde765e70002c049346afbc
DayanMonteiro <dmsmamb@gmail.com>
2023-12-13 13:56:25 -0300

    verify token

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -145,4 +145,10 @@ module.exports = class UserController {
 
     res.status(200).json({ user });
   }
+
+  static async editUser(req, res) {
+    res.status(200).json({
+      message: "Atualizado com sucesso!",
+    });
+  }
 };
---
+++ backend/helpers/verify-token.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+const getToken = require("./get-token");
+
+// middleware to validate token
+const checkToken = (req, res, next) => {
+  console.log("checkToken req", req.headers);
+
+  if (!req.headers.authorization) {
+    return res.status(401).json({ message: "Acesso negado. Sem autorização!" });
+  }
+
+  const token = getToken(req);
+
+  if (!token) {
+    return res.status(401).json({ message: "Acesso negado. Sem Token!" });
+  }
+
+  try {
+    const verified = jwt.verify(token, "nossosecret");
+    req.user = verified;
+    next();
+  } catch (err) {
+    return res.status(400).json({ message: "Acesso negado. Token inválido!" });
+  }
+};
+
+module.exports = checkToken;
