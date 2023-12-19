fc72db8176ea83bb215267db1b129c6ed9ca9a32
DayanMonteiro <dmsmamb@gmail.com>
2023-12-19 14:59:04 -0300

    starting front end

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from "react";
+/* pages */
+import Login from "./components/pages/Auth/Login";
+import Register from "./components/pages/Auth/Register";
+import Home from "./components/pages/Home";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
