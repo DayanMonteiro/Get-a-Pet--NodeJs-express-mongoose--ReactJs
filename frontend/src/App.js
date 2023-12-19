954e091eed0264b0545442eeace2d59856d98364
DayanMonteiro <dmsmamb@gmail.com>
2023-12-19 16:20:20 -0300

    header and footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,9 +5,14 @@ import Login from "./components/pages/Auth/Login";
 import Register from "./components/pages/Auth/Register";
 import Home from "./components/pages/Home";
 
+/* components */
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+
 function App() {
   return (
     <Router>
+      <Navbar />
       <Switch>
         <Route path="/login">
           <Login />
@@ -19,6 +24,7 @@ function App() {
           <Home />
         </Route>
       </Switch>
+      <Footer />
     </Router>
   );
 }
