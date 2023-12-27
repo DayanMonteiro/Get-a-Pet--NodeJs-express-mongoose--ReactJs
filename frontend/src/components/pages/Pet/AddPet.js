065e542b54e12e70eb917b72822a0113a7a6cd79
DayanMonteiro <dmsmamb@gmail.com>
2023-12-27 18:12:38 -0300

    pet creation form

--- frontend/src/components/pages/Pet/AddPet.js
+++ frontend/src/components/pages/Pet/AddPet.js
@@ -5,6 +5,8 @@ import { useHistory } from "react-router-dom";
 
 import styles from "./AddPet.module.css";
 
+import PetForm from "../../form/PetForm";
+
 /* hooks */
 import useFlashMessage from "../../../hooks/useFlashMessage";
 
@@ -14,8 +16,8 @@ function AddPet() {
       <div className={styles.addpet_header}>
         <h1>Cadastre um Pet</h1>
         <p>Depois ele ficará disponível para adoção</p>
+        <PetForm btnText="Cadastrar Pet" />
       </div>
-      <p>Formulário</p>
     </section>
   );
 }
